212b737e2818fcf54253d1ddf97552b230854665
Keith Taylor <58899793+kct2020@users.noreply.github.com>
2024-05-29 16:08:12 +0100

    blog->post

--- src/hive.js
+++ src/hive.js
@@ -18,7 +18,7 @@ export async function getUserAccount(username) {
 }
 
 // Function to fetch posts with specified limit and discussion type
-async function fetchPosts(username, limit, discussionType = "blog") {
+async function fetchPosts(username, limit, discussionType = "post") {
   try {
     const posts = await db.getDiscussions(discussionType, {
       tag: username,
